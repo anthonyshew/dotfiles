5b89cae7825fcafab1defbd9b674d70abe2ca24b
Anthony Shew <anthonyshew@gmail.com>
2026-01-06 15:46:55 -0700

    closea all of neovim if neo-tree is focused when when :q invoked

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 --
+
+-- Quit all of neovim when :q is called in Neo-tree
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "neo-tree",
+  callback = function()
+    vim.keymap.set("n", "q", "<cmd>qa<cr>", { buffer = true, desc = "Quit Neovim" })
+    vim.keymap.set("c", "q<cr>", "qa<cr>", { buffer = true, desc = "Quit Neovim" })
+  end,
+})
